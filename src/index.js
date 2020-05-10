5674b1cec0ee28f1a80a6ce59257df0da122a847
dylandasilva <55394640+dylandasilva1999@users.noreply.github.com>
2020-05-10 19:46:44 +0200

    Initial Commit
    Document setup, changed the title of the application to Spacify Dashboard, imported all the fonts into index.css, removed unneccassary files, added dependencies Bootstrap, Material UI Core and Material UI Icons and removing comments in the default files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './fonts/pacifico-webfont.woff';
+import './fonts/proxima_nova_bold-webfont.woff';
+import './fonts/proxima_nova_thin-webfont.woff';
+import './fonts//proximanova-regular-webfont.woff';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
