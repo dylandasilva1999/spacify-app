24f217ef9f5f0d215f6ecc72b99be367f8549ec2
dylandasilva <55394640+dylandasilva1999@users.noreply.github.com>
2020-05-12 16:15:41 +0200

    Add Basic App Drawer Navigation
    Added the basic app drawer navigation from Material UI, and added the clsx library to the dependencies.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import './components/TopBar'
+import MiniDrawer from './components/TopBar';
+
 function App() {
   return (
     <div>
 
-      <h1 className="logo">Spacify</h1>
+      <MiniDrawer />
 
     </div>
   );
